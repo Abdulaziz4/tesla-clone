2a64abbb33c6b2acb713ec0adeeeda01eb69309c
Abdulaziz Alqahtani <h-xa2012@hotmail.com>
2021-10-15 16:54:59 +0300

    Adding reveal animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
@@ -7,19 +8,36 @@ const Header = () => {
         <img src="/images/logo.svg" alt="" />
       </a>
       <Menu>
-        <a href="1">
-          <p>Model S</p>
-        </a>
-        <a href="1">
-          <p>Model 3</p>
-        </a>
-        <a href="1">
-          <p>Model X</p>
-        </a>
-        <a href="1">
-          <p>Model Y</p>
-        </a>
+        <a href="1">Model S</a>
+        <a href="1">Model 3</a>
+        <a href="1">Model X</a>
+        <a href="1">Model Y</a>
       </Menu>
+      <RightMenu>
+        <a href="1">Shop</a>
+        <a href="1">Account</a>
+        <CustomMenu />
+        <BurgerNav>
+          <li>
+            <a href="1">Existing Inventory</a>
+          </li>
+          <li>
+            <a href="1">Used Inventory</a>
+          </li>
+          <li>
+            <a href="1">Cybertruck</a>
+          </li>
+          <li>
+            <a href="1">Roadster</a>
+          </li>
+          <li>
+            <a href="1">Events</a>
+          </li>
+          <li>
+            <a href="1">Agents</a>
+          </li>
+        </BurgerNav>
+      </RightMenu>
     </Container>
   );
 };
@@ -35,16 +53,37 @@ const Container = styled.div`
   padding: 0 20px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
   flex: 1;
-  p {
+  a {
     font-weight: 600;
     text-transform: uppercase;
-    padding: 0 10px;
+    margin: 0 10px;
+  }
+  @media (max-width: 768px) {
+    display: none;
   }
 `;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin: 0 10px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div``;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 const Section = ({
   title,
@@ -9,15 +10,19 @@ const Section = ({
 }) => {
   return (
     <Wrap img={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <div>
-        <ButtonGroup>
-          <LeftButton>{leftBtnTxt}</LeftButton>
-          {rightBtnTxt && <RightButton>{rightBtnTxt}</RightButton>}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnTxt}</LeftButton>
+            {rightBtnTxt && <RightButton>{rightBtnTxt}</RightButton>}
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </div>
     </Wrap>
